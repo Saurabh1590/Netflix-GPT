65f47a0807ae8ba9d2f9254cc0321159f0c41e7e
Saurabh Sahani <saurabhsahani2710@gmail.com>
2024-10-20 16:27:13 +0530

    validation && redux && auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,43 @@
 import React from "react";
+import { auth } from "../utils/firebase";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector(store => store.user);
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
   return (
-    <div className="absolute px-8 py-3 bg-gradient-to-b from-black z-10">
-      <img className="w-[197px] mx-[100px]"
-      src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-      alt="logo" />
+    <div className="flex justify-between absolute w-screen px-8 py-3 bg-gradient-to-b from-black z-10">
+      <img
+        className="w-[197px] mx-[100px]"
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="logo"
+      />
+      {user && (<div className="flex items-center ">
+        <img
+          className="w-12 h-12 mx-2"
+          src={user?.photoURL}
+          alt="usericon"
+        />
+        <button
+          onClick={handleSignOut}
+          className="bg-[#FF000C] hover:bg-[#ff000dd1] text-white font-bold py-2 px-4 border border-red-700 rounded"
+        >
+          Sign Out
+        </button>
+      </div>)}
     </div>
-  )
+  );
 };
 
 export default Header;
---
+++ src/utils/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDsrfLFn_d8_Za-4Dn188P-rUxi07D6Qpg",
+  authDomain: "netflixgpt-44bf5.firebaseapp.com",
+  projectId: "netflixgpt-44bf5",
+  storageBucket: "netflixgpt-44bf5.appspot.com",
+  messagingSenderId: "709346792655",
+  appId: "1:709346792655:web:5de7675a941dd8d76b93ba",
+  measurementId: "G-6FRRRDLCH4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth();
